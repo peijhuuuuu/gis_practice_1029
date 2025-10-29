ff636d2d64d5d27f304858720e5c3a095170a9ab
peijhuuuuu <callista20050512@gmail.com>
2025-10-29 03:16:13 +0000

    modified:   page_3dmap_2.py

--- page_3dmap_2.py
+++ page_3dmap_2.py
@@ -19,7 +19,7 @@ fig = px.scatter_geo(
     df,
     locations="iso_alpha", 
     locationmode="country names",# 國家代碼
-    color="lifeEXP",      # 依據大陸洲別上色
+    color="lifeExp",      # 依據大陸洲別上色
     hover_name="country",   # 滑鼠懸停時顯示國家名稱
     size="pop",             # 點的大小代表人口數
 
