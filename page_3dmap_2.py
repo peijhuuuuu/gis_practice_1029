845fd55a007c6c177f3ab37c6bcf7c694f8ecf7b
peijhuuuuu <callista20050512@gmail.com>
2025-10-29 03:19:48 +0000

    modified:   page_3dmap_2.py

--- page_3dmap_2.py
+++ page_3dmap_2.py
@@ -17,10 +17,12 @@ df = px.data.gapminder().query("year == 2007")
 # --- 2. 建立 3D 地理散點圖 (scatter_geo) ---
 fig = px.scatter_geo(
     df,
+    locations="iso_alpha",  # 使用 ISO-3 代碼
+    color="lifeExp",
+    hover_name="country",
+    size="pop",
     locations="iso_alpha", 
-    locationmode="country names",# 國家代碼
-    color="lifeExp",      # 依據大陸洲別上色
-    hover_name="country",   # 滑鼠懸停時顯示國家名稱
+    # 滑鼠懸停時顯示國家名稱
     size="pop",             # 點的大小代表人口數
 
     # *** 關鍵：使用 "orthographic" 投影法來建立 3D 地球儀 ***
